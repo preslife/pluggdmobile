e0a4c95dabe93d97019057e39c1203832863f2f4
preslife <118379285+preslife@users.noreply.github.com>
2026-05-01 14:46:13 +0100

    Fix NativeWind styling transform

--- pluggd-mobile/babel.config.js
+++ pluggd-mobile/babel.config.js
@@ -2,6 +2,10 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        plugins: ["react-native-reanimated/plugin"],
+        plugins: [
+            "react-native-css-interop/dist/babel-plugin",
+            ["@babel/plugin-transform-react-jsx", { runtime: "automatic", importSource: "nativewind" }],
+            "react-native-reanimated/plugin",
+        ],
     };
 };
